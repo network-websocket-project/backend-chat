ee923ea70dcb25da5fe97be75b3f05375273a422
nartgg35665 <77729065+nartgg35665@users.noreply.github.com>
2023-04-20 21:49:46 +0700

    Updated

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,72 +1,90 @@
-const asyncHandler=require('express-async-handler')
-const User=require('../models/userModel')
-const generateToken=require('../config/generateToken')
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const generateToken = require("../config/generateToken");
 
-const registerUser=asyncHandler(async(req,res)=>{
-    const {nickname,username,password,avatar}=req.body
+const registerUser = asyncHandler(async (req, res) => {
+  const { nickname, username, password, avatar } = req.body;
 
-    if(!nickname||!username||!password){
-        res.status(400);
-        throw new Error("Please Enter all the Fields")
-    }
+  if (!nickname || !username || !password) {
+    res.status(400);
+    throw new Error("Please Enter all the Fields");
+  }
 
-    const userExists=await User.findOne({username});
+  const userExists = await User.findOne({ username });
 
-    if(userExists){
-        res.status(400);
-        throw new Error("User already exists")
-    }
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
 
-    const user=await User.create({
-        nickname,
-        username,
-        password,
-        avatar,
-    })
-    if(user){
-        res.status(201).json({
-            id:user._id,
-            name:user.nickname,
-            email:user.username,
-            avatar:user.avatar,
-            token:generateToken(user._id)
-        })
-    }else{
-        res.status(400);
-        throw new Error("Failed to create user")
-    }
+  const user = await User.create({
+    nickname,
+    username,
+    password,
+    avatar,
+  });
+  if (user) {
+    res.status(201).json({
+      id: user._id,
+      nickname: user.nickname,
+      username: user.username,
+      avatar: user.avatar,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Failed to create user");
+  }
 });
 
-const authUser=asyncHandler(async(req,res)=>{
-    const {username,password}=req.body;
-    const user=await User.findOne({username});
-    if(user&&(await user.matchPassword(password)) ){
-        res.json({
-            id:user._id,
-            nickname:user.nickname,
-            username:user.username,
-            avatar:user.avatar,
-            token:generateToken(user._id),
-        })
-    }else{
-        res.status(401);
-        throw new Error("Invalid username or password");
-    }
-})
+const authMe = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const user = await User.findOne({ _id});
+  if (user) {
+    res.json({
+      id: user._id,
+      nickname: user.nickname,
+      username: user.username,
+      avatar: user.avatar,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid username or password");
+  }
+});
 
-const allUsers=asyncHandler(async(req,res)=>{
-    const keyword=req.query.keyword?{
-        $or:[
-            {nickname:{$regex:req.query.keyword,$options:"i"}},
-            {username:{$regex:req.query.keyword,$options:"i"}}
-        ]
-    }:{};
-    
-    const users=await User.find(keyword).find({_id:{$ne:req.user._id}})
-    res.send(users);
-})
+const authUser = asyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      id: user._id,
+      nickname: user.nickname,
+      username: user.username,
+      avatar: user.avatar,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid username or password");
+  }
+});
 
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.keyword
+    ? {
+        $or: [
+          { nickname: { $regex: req.query.keyword, $options: "i" } },
+          { username: { $regex: req.query.keyword, $options: "i" } },
+        ],
+      }
+    : {};
 
-module.exports={registerUser,authUser,allUsers}
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
+
+module.exports = { registerUser, authUser, allUsers, authMe };
 
 //.find({_id:{$ne:req.user._id}})
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,12 +4,14 @@ const {
   registerUser,
   authUser,
   allUsers,
+  authMe,
 } = require("../controllers/userControllers");
 
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", authUser);
+router.post('/loginme',protect,authMe)
 router.get("/",protect,allUsers);
 
 module.exports = router;
