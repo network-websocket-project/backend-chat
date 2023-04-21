d6e9eea630930111f5034d0119bd3110c6c008f5
Theerut <theerut.seek@gmail.com>
2023-04-21 13:15:54 +0700

    Change PORT to 30555

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use("/api/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 30555;
 
 const server = app.listen(PORT, console.log(`server started on port ${PORT}`));
 
